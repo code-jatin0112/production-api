ca6293c603d68e27324be5b9fe7831d4861067f1
code-jatin0112 <jatin.kumar.s.125@kalvium.community>
2026-02-23 11:40:12 +0530

    Updated Swagger Live URL

--- swagger.js
+++ swagger.js
@@ -9,10 +9,10 @@ const options = {
       version: "1.0.0",
     },
     servers: [
-      {
-        url: "http://localhost:3000",
-      },
-    ],
+  {
+    url: "https://production-api.onrender.com",
+  },
+],
   },
   apis: ["./routes/*.js"],
 };
